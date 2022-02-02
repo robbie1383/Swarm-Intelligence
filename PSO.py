520e987ada1c0f8db4882f8ce265563b3736d7b3
WangPeng <wangpeng930204@gmail.com>
2022-02-02 16:22:06 +0100

    remove comments

--- PSO.py
+++ PSO.py
@@ -1,7 +1,9 @@
 from Particle import Particle
 
+
 def distance(p1: Particle, p2: Particle):
     return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
 
 class PSO:
 
@@ -40,24 +42,3 @@ class PSO:
                     gbest = self.neighbours[particleIndex][i]
             # Move based on PSO rules
             self.particles[particleIndex].move(self.particles[gbest], a)
-
-"""
-This one gives errors and i can't really figure out what you did so I don't really know how to fix it
-    def updateNeighbours(self):
-        for i in range(len(self.particles)):  # find neighbours for each Particle i
-            longest = 0
-            for j in range(len(self.particles)):  # compare each neighbour j distance to i
-                if i != j:
-                    if len(self.neighbours) >= i:  # already have neighbours
-                        for p in self.neighbours[i]:
-                            if self.distance(self.particles[i], p) > longest:
-                                longest = self.distance(self.particles[i], p)
-                        if (self.distance(self.particles[i], self.particles[j])) < longest:
-                            self.neighbours[i].pop()
-                            self.neighbours[i].append(j)  # update longest neighbour
-                            longest = self.distance(self.particles[i], self.particles[j])
-                    else:  # first time run, no neighbours
-                        if self.distance(self.particles[i], self.distance(self.particles[j])) > longest:
-                            longest = self.distance(self.particles[i], p)
-                            self.neighbours[i].append(j)
-"""
